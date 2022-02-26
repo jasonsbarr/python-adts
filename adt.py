8ad44a945b764f4bce9172e7ed83c4bc8eca1252
Jason Barr <jason.barr@metronetinc.com>
2022-02-25 23:40:06 -0600

    Fix some bugs, probably not all of them

--- adt.py
+++ adt.py
@@ -8,6 +8,20 @@ class ADT(ABC):
     def __delattr__(self):
         raise ValueError("Cannot delete values on an ADT instance")
 
+# set the __value__ property on an instance without its own fields
+def set_instance_value(instance, *args):
+    if len(args) == 1:
+        setattr(instance, "__value__", args[0])
+    elif len(args) > 1:
+        setattr(instance, "__value__", args)
+
+    @property
+    def _get_value(self):
+        return self.__value__
+
+    setattr(instance, value, _get_value)
+
+
 # variants is a list of dictionaries that match the one returned by create_variant
 # members are data attributes and methods, including dunder methods, to be attached to the type representative
 # do not include variant names as method names, as these will be created as methods on the type representative
@@ -28,13 +42,19 @@ def create_adt(type_name, *, variants=None, members=None):
 
         members["__init_subclass__"] = init_subclass
 
-    tyrep = type(type_name, (ADT,), members)
-    gs = globals()
+    if variants is None:
+        if not "__init__" in members:
+            def init(self, *args):
+                set_instance_value(self, args)
 
-    if variants not none:
+            members["__init__"] = init
+
+    tyrep = type(type_name, (ADT,), members)
+
+    if variants not None:
+        gs = globals()
         for variant in variants:
             gs[variant["name"]] = type(variant["name"], (tyrep,), variant["members"])
-
 
     return tyrep
 
@@ -48,13 +68,20 @@ def create_variant(variant_name, *, fields=None, members=None):
     if members is None:
         members = {}
 
-    if fields not None:
+    if not "__init__" in members:
         def init(self, *args, **kwargs):
-            for k, v in kwargs.items():
-                setattr(self, k, v)
+            if fields not None:
+                for k, v in kwargs.items():
+                    setattr(self, k, v)
 
-            for i, f in enumerate(args):
-                setattr(self, i, f)
+                for i, v in enumerate(args):
+                    setattr(self, fields[i], v)
+
+            else:
+                if len(args) == 1:
+                    setattr(self, "__value__", args[0])
+                elif len(args) > 1:
+                    setattr(self, "__value__", args)
 
         members["__init__"] = init
 
