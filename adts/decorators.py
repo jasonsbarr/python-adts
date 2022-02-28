f7d5d599fc66be6ce9db725bf25caa2a85e6ea50
Jason Barr <jason.barr@metronetinc.com>
2022-02-28 17:05:24 -0600

    Remove curry_method decorator

--- adts/decorators.py
+++ adts/decorators.py
@@ -13,16 +13,3 @@ def curry(fn):
         return curry(partial(fn, *args, **kwargs))
 
     return curried
-
-
-def curry_method(fn):
-    def curried(*args, **kwargs):
-        argspec = inspect.getfullargspec(fn)
-        all_args_count = len(
-            argspec.args) + len(argspec.kwonlyargs) - len(argspec.kwonlydefaults.keys())
-
-        if all_args_count >= all_args_count:
-            return fn(*args, **kwargs)
-        return curry_method(partial(fn, *args, **kwargs))
-
-    return curried
